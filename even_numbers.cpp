31dca3c20364d4705c8b57b68ff592c47299e088
HovorMosesKwame <102967940+HovorMosesKwame@users.noreply.github.com>
2022-08-07 22:18:42 +0000

    Update even_numbers.cpp

--- even_numbers.cpp
+++ even_numbers.cpp
@@ -4,17 +4,19 @@ using namespace std;
 int main (){
     int num;
     int i = 1;
-    int sum;
+    int sum = 0;
 
     cout << "enter a number: ";
     cin >> num;
-
+/*Odd numbers can be found by letting the if statement's condition to be 1*/
     while (i < num){
         if (i%2 == 0){
-            sum = sum +1;
+            sum = sum +i;
         }
     i= i+1;
     }
 
     cout <<"The sum of even numbers less than " <<num<< " is " <<sum;
+    
+    return 0;
 }
